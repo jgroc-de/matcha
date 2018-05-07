1f56def6f1f74ac1efc31840a6b768abf6a3e4f3
jgroc-de <jgroc-de@student.42.fr>
2018-05-07 16:17:02 +0200

    first step slim

--- src/dependencies.php
+++ src/dependencies.php
@@ -6,7 +6,17 @@ $container = $app->getContainer();
 // view renderer
 $container['renderer'] = function ($c) {
     $settings = $c->get('settings')['renderer'];
-    return new Slim\Views\PhpRenderer($settings['template_path']);
+    // for php-view
+    //return new Slim\Views\PhpRenderer($settings['template_path']);
+    
+    //for twig view
+    $view = new Slim\Views\Twig($settings['template_path']);
+
+    $router = $c->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+
+    return $view;
 };
 
 // monolog
@@ -16,4 +26,13 @@ $container['logger'] = function ($c) {
     $logger->pushProcessor(new Monolog\Processor\UidProcessor());
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
+};
+
+// PDO
+$container['db'] = function ($c) {
+    $db = $c->get('settings')['db'];
+    $pdo = new PDO('mysql=' . $db['host'] . ';dbname=' . $db['dbname'], $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
 };
--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,13 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        // Database settings
+        'db' => [
+            'host' => 'localhost/www/matcha',
+            'user' => 'user',
+            'pass' => 'secret',
+            'dbname' => 'slim',
+        ],
     ],
 ];
