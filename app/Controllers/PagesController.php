9dba0089a0a7a1f07b032d32c57d4c44c98c50fb
jgroc-de <jgroc-de@student.42.fr>
2018-05-10 14:26:50 +0200

    up

--- app/Controllers/PagesController.php
+++ app/Controllers/PagesController.php
@@ -2,15 +2,30 @@
 
 namespace App\Controllers;
 
+/**
+ * class PagesController
+ * this class is called by each routes
+ */
 class PagesController
 {
+    /**
+     * @var array : for $container
+     */
     private $container;
 
+    /**
+     * @param $container array
+     */
     public function __construct($container)
     {
         $this->container = $container;
     }
-    
+
+    /**
+     * @param $request requestInterface : route for home
+     * @param $response responseInterface
+     * @return twig view
+     */
     public function home ($request, $response)
     {
         if (isset($_SESSION['id']))
@@ -19,11 +34,22 @@ class PagesController
             return $this->view->render($response, $dir = 'templates/login.html.twig');
     }
     
+    /**
+     * @param $request requestInterface : route for signup
+     * @param $response responseInterface
+     * @return twig view
+     */
     public function signup ($request, $response)
     {
-        return $this->container->view->render($response, 'templates/sign.html.twig');
+        $characters = ['Rick', 'Morty', 'Beth', 'Jerry', 'Summer'];
+        return $this->container->view->render($response, 'templates/sign.html.twig', ['characters' => $characters]);
     }
 
+    /**
+     * @param $request requestInterface : route for login
+     * @param $response responseInterface
+     * @return twig view
+     */
     public function login ($request, $response)
     {
         $params = $request->getParams();
@@ -34,6 +60,10 @@ class PagesController
         return $this->view->render($response, $dir = 'templates/login.html.twig');
     }
 
+    /**
+     * @param $name string : shortcut to access dependencies in $container
+     * @return $container['$name'] : matching class from container if any
+     */
     public function __get($name)
     {
         return $this->container->get($name);
