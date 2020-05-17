79b10a91d670c41feabc31725c2a64bce4b2f5f2
jgroc-de <jgroc-de@student.42.fr>
2020-05-17 22:49:24 +0200

    maj

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ sudo apt install -y php-zmq sendmail golang-go
 go get github.com/mailhog/MailHog
 go get github.com/mailhog/mhsendmail
 composer install
-sudo echo 'sendmail_path = /home/user42/go/bin/mhsendmail' > /etc/php/7.2/cli/php.ini
+sudo echo 'sendmail_path = /home/user42/go/bin/mhsendmail' >> /etc/php/7.2/cli/php.ini
 sudo mysql < conf_user.sql
 echo 'launch mail server with command ~/go/bin/mailhog\n'
 echo 'launch webserver with command php -S localhost:8080 -t public'
