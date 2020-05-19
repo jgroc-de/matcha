cbd2c3a22dd3ab99fa0ae648cac4e6345e662d0d
jgroc-de <jgroc@ccmbenchmark.com>
2020-05-19 16:08:50 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-sudo apt install -y php-zmq golang-go
+sudo apt install -y php-zmq golang-go composer
 go get github.com/mailhog/MailHog
 go get github.com/mailhog/mhsendmail
 composer install
-sudo echo 'sendmail_path = /home/user42/go/bin/mhsendmail' >> /etc/php/7.2/cli/php.ini
+cd ratchet && composer install && cd ..
+echo 'sendmail_path = /home/user42/go/bin/mhsendmail' | sudo tee -a /etc/php/7.2/cli/php.ini
 sudo mysql < conf_user.sql
 echo 'launch mail server with command ~/go/bin/MailHog'
 echo 'launch websocket server with command php ./ratchet/index.php'
