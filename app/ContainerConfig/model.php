66d2fe53e4458d8920ba4d777c2758575c41ac96
jgroc-de <jgroc@ccmbenchmark.com>
2020-05-24 00:20:01 +0200

    [heroku] test

--- app/ContainerConfig/model.php
+++ app/ContainerConfig/model.php
@@ -13,10 +13,20 @@ use Psr\Container\ContainerInterface;
 
 /** @var ContainerInterface $container */
 $container['db'] = function ($container) {
-    $db = $container->get('settings')['db'];
-    $pdo = new \PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'], $db['user'], $db['pass']);
-    $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+    try {
+        $db = $container->get('settings')['db'];
+        $pdo = new \PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'], $db['user'], $db['pass']);
+        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+    } catch (\PDOException $error) {
+        try {
+            $pdo = new \PDO('mysql://vyuqi2ey7r0q5q3r:scyksdsel0daocy9@e7qyahb3d90mletd.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/srnidj0gjae99zx7');
+            $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+        } catch (\PDOException $error) {
+            $pdo = null;
+        }
+    }
 
     return $pdo;
 };
