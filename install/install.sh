bf550b2356fcbd526589edb82f52aaeb6fc916d0
jgroc-de <jgroc@ccmbenchmark.com>
2020-06-08 23:28:14 +0200

    [install] fix

--- install/install.sh
+++ install/install.sh
@@ -3,9 +3,9 @@ go get github.com/mailhog/MailHog
 go get github.com/mailhog/mhsendmail
 composer install
 sudo mysql < conf_user.sql
-cd .. && cd ratchet && composer install && cd ..
+cd ../ratchet && composer install
 echo 'sendmail_path = /home/user42/go/bin/mhsendmail' | sudo tee -a /etc/php/7.2/cli/php.ini
-mkdir public/user_img
+mkdir ../public/user_img
 echo 'launch mail server with command ~/go/bin/MailHog'
 echo 'launch websocket server with command php ./ratchet/index.php'
 echo 'launch webserver with command php -S localhost:8080 -t public'
